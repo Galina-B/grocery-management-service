fbee090dc4ff9e3f582562d4dcf94f1d56baac05
Galina-B <galina.besler@yahoo.com>
2023-02-27 15:25:00 +0200

    Add basic project structure and spring web, jpa, h2 dependencies;

---
+++ src/main/java/com/gms/GroceryManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryManagementServiceApplication.class, args);
+	}
+
+}
