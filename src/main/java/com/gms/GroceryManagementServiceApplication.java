4b7161d7426b70034690dc0a5df4edff48689dfc
Galina-B <galina.besler@yahoo.com>
2023-02-27 16:02:07 +0200

    Add lombok and logback setup;

--- src/main/java/com/gms/GroceryManagementServiceApplication.java
+++ src/main/java/com/gms/GroceryManagementServiceApplication.java
@@ -1,13 +1,16 @@
 package com.gms;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class GroceryManagementServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GroceryManagementServiceApplication.class, args);
+		logger.info("Grocery Management Service is running.");
 	}
 
 }
