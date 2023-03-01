2b68da9b259c5be19ec86f907134feae99c7d610
Galina-B <galina.besler@yahoo.com>
2023-03-01 15:34:04 +0200

    Rename spring boot app class;

--- src/main/java/com/gms/Application.java
+++ src/main/java/com/gms/Application.java
@@ -6,11 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
 @SpringBootApplication
-public class GroceryManagementServiceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GroceryManagementServiceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 		logger.info("Grocery Management Service is running.");
 	}
-
 }
